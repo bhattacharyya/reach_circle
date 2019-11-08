b20023eedf12b0c6a3554e8ac9633eeb7602fe27
Shantanu Bhattacharyya <shantanu@locus.sh>
2019-11-08 22:11:27 +0530

    bug fix for loss every few games

--- reach_circle_commandline.py
+++ reach_circle_commandline.py
@@ -7,7 +7,7 @@ import numpy as np
 alpha = 0.9
 gamma = 1.0
 epsilon = 0.0
-episodes = 1000
+episodes = 10000
 
 mov_list = [-20,-10,10,20]
 steps = 0
@@ -46,9 +46,6 @@ def movement(): # Movement of Green Ball
 	pos_y1 = 80 
 	x1 = 10
 	y1 = 10
-	prev_move = (0,0)
-	prev_dist_centre = 1000000
-
 	game = 1
 	reward = 0
 
@@ -57,29 +54,26 @@ def movement(): # Movement of Green Ball
 		reward = 0
 		state = list(state_space.keys())[list(state_space.values()).index((pos_x1, pos_y1))]
 		steps += 1
-		#reward = -1
-		if game % episodes == 0:	
+
+		if game == episodes:	
 			end = 0
 			np.savetxt("qtable.csv", q_table, delimiter=",")
 			fout.write("qtable saved at " + str(steps) + " steps\n")
+			print("saved qtable")
 
 		if steps % 200 == 0:
 			checkpoint = steps
 			game += 1
-			print("Game : "+str(self.game) + " lost")
+			print("Game : "+str(game) + " lost")
 			fout.write(str(steps) + " adjusting, time up\n")
 			fout.write("game "+str(game) + " lost\n")
 			fout.flush()
 			new_x1 = (50 - pos_x1)
 			new_y1 = (80 - pos_y1)
-			#canvas.move(circle, new_x1, new_y1)
 			pos_x1 += new_x1
 			pos_y1 += new_y1		
 
-		if steps % 1000 == 0:
-			print("finished game : ",game)
-
-		if game < 2000000:
+		if game < 20000000: #arbitrarily chosen high value
 			x1 = random.choice(mov_list)
 			y1 = random.choice(mov_list)
 			action = list(action_space.keys())[list(action_space.values()).index((x1, y1))]
@@ -92,7 +86,7 @@ def movement(): # Movement of Green Ball
 			else:
 				action = np.argmax(q_table[state])
 				x1, y1 = action_space[action]
-			#print("suggested",x1, y1)
+
 
 		fout.write("Moving by "+str(x1)+", "+str(y1))
 
@@ -105,19 +99,15 @@ def movement(): # Movement of Green Ball
 		if pos_y1 < 25:
 			y1 = 10  
 
-
 		pos_x1 += x1
 		pos_y1 += y1
-
-		next_state = list(state_space.keys())[list(state_space.values()).index((pos_x1, pos_y1))]
-		next_max = np.max(q_table[next_state])
 
 		fout.write("Now at "+str(pos_x1)+", "+str(pos_y1)+"\n")
 
 		if pos_x1 in [240,250,260] and pos_y1 in [170,180,190]:
 			reward = 100
 			game += 1
-			print("Game : "+str(self.game) + " won")
+			print("Game : "+str(game) + " won")
 			fout.write(str(steps) + " adjusting, target found "+str(pos_x1) +", " + str(pos_y1) + "\n")
 			steps = checkpoint
 			fout.write("Resetting to checkpoint steps "+str(checkpoint)+"\n")
@@ -125,17 +115,16 @@ def movement(): # Movement of Green Ball
 			fout.flush()
 			new_x1 = (50 - pos_x1)
 			new_y1 = (80 - pos_y1)
-			#canvas.move(circle, new_x1, new_y1)
 			pos_x1 += new_x1
 			pos_y1 += new_y1
 
 		fout.write("Reward for the step : "+str(reward)+"\n")
-		old_q_value = q_table[state, action]
 		
-		new_value = old_q_value + alpha * (reward + gamma * next_max)
+		old_q_value = q_table[state, action]
+		next_state = list(state_space.keys())[list(state_space.values()).index((pos_x1, pos_y1))]
+		next_max = np.max(q_table[next_state])
 		q_target = reward + gamma * next_max
 		q_delta = q_target - old_q_value
 		q_table[state, action] = old_q_value + alpha * q_delta
-
 
 movement()
